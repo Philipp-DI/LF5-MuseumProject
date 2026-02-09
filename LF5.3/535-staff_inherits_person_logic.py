7626bf0431c78b6f11a4b5a2905f0abf934c4620
Philipp-DI <philipp.steinmann@edu.dualis-institut.de>
2026-02-09 13:42:57 +0100

    task for public method and private attribute done

--- LF5.3/535-staff_inherits_person_logic.py
+++ LF5.3/535-staff_inherits_person_logic.py
@@ -5,17 +5,37 @@ class Person:
         self.name: str = name
         self.birth_year: int = birth_year
         self.theoretical_age: int = datetime.datetime.now().year - birth_year
-
+    
+    def get_private_info(self):
+        private = {k: v for k, v in self.__dict__.items() if k.startswith("_")}
+        return "\n".join(f"{k}: {v}" for k, v in private.items()) if private else "No private info"
 
 class MuseumStaff(Person):
-    staff_role: dict = {0: "Administrator", 2: "Researcher", 3: "Archivist"}
+    staff_role: dict = {0: "Administrator", 1: "Curator", 2: "Researcher", 3: "Archivist"}
     
     def __init__(self, name: str, birth_year: int, staff_role: int):
         super().__init__(name, birth_year)
-        self.staff_role = self.staff_role[staff_role]
+        self._staff_role = self.staff_role[staff_role]
+        
+    def __str__(self):
+        public = {k: v for k, v in self.__dict__.items() if not k.startswith("_")}
+        return "\n".join(f"{k}: {v}" for k, v in public.items())
+        # return f"{self.name} ({self.theoretical_age}) - {self._staff_role}"
+    
+    def get_private_info(self):
+        private = {k: v for k, v in self.__dict__.items() if k.startswith("_")}
+        return "\n".join(f"{k}: {v}" for k, v in private.items()) if private else "No private info"
 
 
 # global variables for testing purposes
 person1 = Person("Alice", 1990)
 person2 = Person("Bob", 1985)
 person3 = MuseumStaff("Karl", 1976, 0)
+person4 = MuseumStaff("Jochen", 1878, 3)
+
+# testing outputs
+print("printing public info only:\n")
+print(person3)
+print("-" * 30)
+print("printing private info if available:\n")
+print(person2.get_private_info())
