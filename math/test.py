2045caa8ca3c26f15b9ab6ee2c1fc360de413dd2
Philipp-DI <philipp.steinmann@edu.dualis-institut.de>
2026-02-19 13:49:08 +0100

    test commit

---
+++ math/test.py
@@ -0,0 +1,9 @@
+zahl1 = input("Erste Zahl eingeben: ")
+zahl2 = input("Zweite Zahl eingeben: ")
+
+#zahl1 = float(zahl1)
+#zahl2 = float(zahl2)
+
+ergebnis = zahl1 + zahl2
+
+print("Das Ergebnis ist:", ergebnis)
